4fc8fef7c2799b0d9d8bb350cc2a5326d8c65884
lorenzo chahid <clkwebdeveloper@gmail.com>
2024-02-13 10:28:36 +0100

    change database url

--- database.py
+++ database.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-DATABASE_URL = "postgresql://retviews:password999@localhost/postgres"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
